448ccf69733e4611264cd0abacd73bbaaab9d237
David Heyman <dave@axismaps.com>
2022-09-10 09:41:13 +0100

    Initial updates

--- src/pages/thank-you.jsx
+++ src/pages/thank-you.jsx
@@ -6,7 +6,7 @@ export default function ThankYou() {
   return (
     <>
       <Head>
-        <title>You’re subscribed - Spencer Sharp</title>
+        <title>You’re subscribed - David Heyman</title>
         <meta
           name="description"
           content="Thanks for subscribing to my newsletter."
